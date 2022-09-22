943cd3c11e7bad632398df2015c19715f0001849
Hasan Yüksektepe <hasan.yuksektepe@ogr.iu.edu.tr>
2022-09-22 22:21:02 +0300

    1.0.4
    - Kategori komisyonları eklendi

--- src/trendyol/Marketplace/TrendyolMarketplaceCategories.php
+++ src/trendyol/Marketplace/TrendyolMarketplaceCategories.php
@@ -27,8 +27,32 @@
         }
 
         public function get_category_info($category_id = null){
-            $url    = 'https://api.trendyol.com/sapigw/product-categories/'.$category_id.'/attributes';
-            $result = $this->request()->get($url);
+            $category_info_json = json_decode(file_get_contents((__DIR__).'/../assets/category_info.json'), true);
+            $keys = $this->trendyol_array_search($category_info_json['Categories'], 'Id', $category_id);
+            $url                = 'https://api.trendyol.com/sapigw/product-categories/'.$category_id.'/attributes';
+            $result             = $this->request()->get($url);
+            $result->commission = $keys['Commission'];
             return $result;
         }
+
+        public $trendyol_array_search_result = null;
+        public function trendyol_array_search($data = [], $key = null, $value = null){
+            if(isset($data) and $data != null){
+                foreach($data as $category){
+
+                    if(isset($category[$key]) and $category[$key] == $value){
+                        $this->trendyol_array_search_result = $category;
+                    }
+
+                    if(isset($category['Nodes']) and $category['Nodes'] != null){
+                        $this->trendyol_array_search($category['Nodes'], $key, $value);
+                    }
+
+                }
+            }else{
+                return 2;
+            }
+            return $this->trendyol_array_search_result;
+        }
+
     }
